ba20c8c9c01e2e7f4b4a582187c3a0621e1ab6ac
wuhandaxuevip <wuhandaxuevip@163.com>
2021-06-30 23:43:55 +0800

    first commit

---
+++ MybatisConfig.java
@@ -0,0 +1,25 @@
+package com.javasm.springbootmybatis.config;
+
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author：whl
+ * @Version：1.0
+ * @Date：2021/6/29-16:39
+ * @Since:jdk1.8
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(){
+        return new ConfigurationCustomizer() {
+            @Override
+            public void customize(org.apache.ibatis.session.Configuration configuration) {
+                configuration.setMapUnderscoreToCamelCase(true);
+            }
+        };
+    }
+}
